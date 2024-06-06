d370d06cd637fde29b0a637b9f2e79136eddae04
nolen <nolensky@mail.ru>
2024-06-06 20:01:07 +0300

    HW_12

--- src/App.java
+++ src/App.java
@@ -2,8 +2,8 @@ public class App {
     public static void main(String[] args) {
         Author AlexPushkin = new Author("Александр", "Пушкин");
         Author LevTolstoy = new Author("Лев", "Толстой");
-        Book book = new Book("Сказка о рыбаке и рыбке", new Author(AlexPushkin.getFirstName(), AlexPushkin.getLastName()), 1835);
-        Book book2 = new Book("Война и мир", new Author(LevTolstoy.getFirstName(), LevTolstoy.getLastName()), 1868);
+        Book book = new Book("Сказка о рыбаке и рыбке",AlexPushkin, 1835);
+        Book book2 = new Book("Война и мир",LevTolstoy, 1868);
     }
 }
 
